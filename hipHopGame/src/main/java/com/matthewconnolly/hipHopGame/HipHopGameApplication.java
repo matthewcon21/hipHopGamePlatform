31c3cb0692640b94e31091690915689bbf3b14d2
matthewcon21 <matthewcon21@gmail.com>
2026-03-23 01:32:36 +0000

    add spring boot project structure

---
+++ hipHopGame/src/main/java/com/matthewconnolly/hipHopGame/HipHopGameApplication.java
@@ -0,0 +1,13 @@
+package com.matthewconnolly.hipHopGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipHopGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HipHopGameApplication.class, args);
+	}
+
+}
---
+++ hipHopGame/src/test/java/com/matthewconnolly/hipHopGame/HipHopGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewconnolly.hipHopGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipHopGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
